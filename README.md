30ca61f79b6248a9c4f55d497d0b82a75099448b
foglar <kohout19fi@gymstola.cz>
2023-03-15 19:32:41 +0100

    Added README and files for project
    First release

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
 # safeMail
+
+## Quick startguide
+
+:warning: **WARNING** This app is not safe to use if you need security, because it uses student version of RSA encryption, which is not safe to use in real life. It is only for educational purposes, and it is not recommended to use it for real secret communications.
+
+### Installation
+
+1. Download the latest version of safeMail from [here][releases] and unzip it.
+2. Download all required libraries with ```pip3 install -r requirements.txt``` (ezgmail, pyperclip, pyinputplus, rich)
+3. Create your own public and private keys by running ```python3 makePublicPrivateKeys.py``` save them in the project folder and name them **my_pubkey.txt** and **my_privkey.txt** (:warning:DON'T SHARE private key with anyone) and send your public key to your friends.
+4. You have to create your email adress at [gmail](https://www.gmail.com) Then go on [this](https://developers.google.com/gmail/api/quickstart/python) pick option **Enable Gmail API** and fill form. After that you should be able to download file **credentials.json** and upload it to the project folder. It contents your gmail authentication key and code, they act like your gmail password, so be carefull with them.
+5. Then you can run ```python3 safeMail.py```, first time the window will pop up and askes you for login to your gmail. This app is not verified by Google, so it will try to stop from using it. Just click **advanced settings** and **continue (unsafe)**. (Consider creating new inbox and some new password only for this project)
+6. Now you can read your emails with pressing enter or **v**, terminal will look like this:
+
+   ```bash
+      $ python safeMail.py
+                  __    ___  ___      _ _ 
+                 / _|   |  \/  |     (_) |
+       ___  __ _| |_ ___| .  . | __ _ _| |
+      / __|/ _` |  _/ _ \ |\/| |/ _` | | |
+      \__ \ (_| | ||  __/ |  | | (_| | | |
+      |___/\__,_|_| \___\_|  |_/\__,_|_|_|, by foglar
+
+      To view your emails, press ENTER > 
+
+   ```
+
+7. For more commands type **help** or **h**.
+
+### Functions
+
+- **q** or **quit** - quits the program
+- **help** or **h** - shows help
+
+- **ENTER** or **v** or **view** - shows emails
+- **r** - read one email, after pressing enter you will be asked for email number
+- **s** or **send** - send email, after pressing enter you will be asked for email adress and message
+- **nk** or **newkey** - create new public and private keys
+
+## How it works
+
+### Cipher
+
+- safeMail uses ezgmail library from Al Sweigart to read and send emails and it is builded on the code of his book **Automate the boring stuff with Python**. It is using public key cipher and RSA encryption from his book **Cracking Codes with Python**.
+- How keys work:
+  1. You create your own public and private keys with **makePublicPrivateKeys.py** or **nk** command in safeMail.
+  2. You send your public key to your friends or contacts.
+  3. When they want to send you email, safeMail encrypt it with your public key and then send it to you.
+  4. When you want to read your email, safeMail decrypt it with your private key, so you can read it.
+  5. So no one can read your emails, because they are encrypted with your public key and only you can decrypt them with your private key, you can share your public key with anyone, but keep your private key safe.
+
+:warning: **WARNING** This app is not safe to use if you need security, because it uses student version of RSA encryption, which is not safe to use in real life. It is only for educational purposes, and it is not recommended to use it for real secret communications.
+
+### Folders and files
+
+- **safeMail** - main folder <br/>
+  |- **backup** - folder for backuping your public and private keys if you accidentaly delete them<br/>
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|- **my_privkey_backup_????-??-??-??:??:??.txt** - your public key with time when you delete him<br/>
+  |- **cache** - folder for caching your encrypted emails (we save only encrypted emails, so no one can read them anyway, and delete them after you exit the program)<br/>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|- **emailSend.txt** - your last sended encrypted email (files are deleted after you leave<br/>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|- **cache?.txt** - your last 10 encrypted emails from inbox (files are deleted after you leave)<br/>
+  |- **keys** - folder for storing your public keys of your contacts<br/>
+  |- **safeMail.py** - main file running the program<br/>
+  |- **makePublicPrivateKeys.py** - file for creating public and private keys<br/>
+  |- **my_pubkey.txt** - your public key<br/>
+  |- **my_privkey.txt** - your private key<br/>
+  |- **credentials.json** - your gmail authentication key and code<br/>
+  |- **requirements.txt** - required libraries<br/>
+  |- **README.md** - this file<br/>
+
+[releases]: https://www.github.com/foglar/safeMail/releases
---
+++ safeMail.py
@@ -0,0 +1,250 @@
+#! python3
+# safeMail.py - Read and send encrypted emails
+
+# TODO: Add option to pick from multiple keys when writing email
+
+from rich.console import Console
+from datetime import datetime as d
+from pathlib import Path
+import pyinputplus as pyip
+import os
+import sys
+import shutil
+import ezgmail
+import publicKeyCipher
+import makePublicPrivateKeys
+
+console = Console()
+
+try:
+    ezgmail.init()
+except Exception:
+    console.print(
+        "Gmail login failed, please check your credentials in credentials.json",
+        style="bold red",
+    )
+    console.print(Exception, style="red")
+    console.print(
+        "More information about program on https://www.github.com/foglar/safeMail/",
+        style="bold blue",
+    )
+    sys.exit()
+
+encryptedMessages = []
+
+
+def main():
+    logo()
+    while True:
+        try:
+            read_or_send = console.input(
+                "To view your emails, press [bold red]ENTER[/bold red] > "
+            )
+        except KeyboardInterrupt:
+            os.system("clear")
+            console.print("\nBye!", style="bold red")
+            console.print(
+                "More information about program on https://www.github.com/foglar/safeMail/",
+                style="bold blue",
+            )
+            sys.exit()
+        print("")
+        # View emails
+        if (
+            read_or_send == ""
+            or read_or_send.lower().startswith("v")
+            or read_or_send.lower().startswith("view")
+        ):
+            senders, emails, timestamps = readEmails()
+            os.system("clear")
+            for i in range(len(emails)):
+                preview = emails[i][:50].replace("\n", " ")
+                console.print(
+                    f"Email from : [bold red] {senders[i]} [/bold red]\tTime: [bold red] {timestamps[i]} [/bold red]\tIndex: {i+1}"
+                )
+                console.print(f"Message    : [bold cyan] {preview}... [/bold cyan]")
+                console.print("")
+        # Read email
+        elif read_or_send.lower().startswith("r") or read_or_send.lower().startswith(
+            "read"
+        ):
+            emailSender, emailBody, emailTime = readEmail()
+            os.system("clear")
+            console.print(
+                f"Email from : [bold red] {emailSender} [/bold red]\tTime: [bold red] {emailTime} [/bold red]"
+            )
+            console.print(f"Message    : [bold cyan] {emailBody} [/bold cyan]")
+            console.print("")
+        # Send email
+        elif read_or_send.lower().startswith("s") or read_or_send.lower().startswith(
+            "send"
+        ):
+            os.system("clear")
+            sendEmails()
+        # Help
+        elif read_or_send.lower().startswith("h") or read_or_send.lower().startswith(
+            "help"
+        ):
+            os.system("clear")
+            logo()
+            console.print(
+                "Simple tool for safe communication with friends via gmail, messages are encrypted with public key cipher",
+                style="bold blue",
+            )
+            console.print(
+                "\t- To view your emails, press [bold red]ENTER[/bold red]",
+                style="bold cyan",
+            )
+            console.print(
+                "\t- To read an email, press [bold red]r[/bold red]", style="bold cyan"
+            )
+            console.print(
+                "\t- To send an email, press [bold red]s[/bold red]", style="bold cyan"
+            )
+            console.print(
+                "\t- To quit, press [bold red]q[/bold red]", style="bold cyan"
+            )
+            console.print(
+                "\t- Create new public and private keys with [bold red]nk[/bold red], or [bold red]newkeys[bold red]\n",
+                style="bold cyan",
+            )
+            console.print(
+                "More information about program on https://www.github.com/foglar/safeMail/\n",
+                style="bold blue",
+            )
+        # New keys
+        elif read_or_send.lower().startswith("nk") or read_or_send.lower().startswith(
+            "newkeys"
+        ):
+            makeNewKeys()
+        # Exit program
+        elif read_or_send.lower().startswith("q") or read_or_send.lower().startswith('quit'):
+            os.system("clear")
+            for i in range(11):
+                if os.path.exists(f"cache/email{i}.txt"):
+                    os.remove(f"cache/email{i}.txt")
+            if os.path.exists('cache/sendEmail.txt'):
+                os.remove("cache/sendEmail.txt")
+            console.print("Cache cleared!", style="bold red")
+            console.print("Bye!", style="bold red")
+            console.print(
+                "More information about program on https://www.github.com/foglar/safeMail/",
+                style="bold blue",
+            )
+            sys.exit()
+
+
+def readEmails():
+    senders = []
+    emails = []
+    timestamps = []
+    emailsEncrypted = searchEmails()
+    for i in range(len(emailsEncrypted)):
+
+        emailFile = open(f"cache/email{i}.txt", "w")
+        emailFile.write(emailsEncrypted[i].messages[0].body)
+        emailFile.close()
+
+        emailsDecrypted = decryptEmails(f"cache/email{i}.txt", "my_privkey.txt")
+        emails.append(emailsDecrypted)
+        senders.append(emailsEncrypted[i].messages[0].sender)
+        timestamps.append(emailsEncrypted[i].messages[0].timestamp)
+
+    return senders, emails, timestamps
+
+
+def readEmail():
+    emailEncrypted = searchEmails()
+
+    emailNum = pyip.inputInt("Email number > ", max=len(emailEncrypted))
+    emailNum -= 1
+
+    emailFile = open(f"cache/email{emailNum}.txt", "w")
+    emailFile.write(emailEncrypted[emailNum].messages[0].body)
+    emailFile.close()
+
+    emailDecrypted = decryptEmails(f"cache/email{emailNum}.txt", "my_privkey.txt")
+    emailBody = emailDecrypted
+    emailSender = emailEncrypted[emailNum].messages[0].sender
+    emailTime = emailEncrypted[emailNum].messages[0].timestamp
+
+    return emailSender, emailBody, emailTime
+
+
+def sendEmails():
+    recipient = pyip.inputEmail("To: ")
+    publicKey = pyip.inputFilepath("Public key path: ")
+    message = console.input("Message: ")
+
+    emailFile = open("cache/sendEmail.txt", "w")
+    emailFile.write(message)
+    emailFile.close()
+
+    messageEncrypted = encryptEmails("cache/sendEmail.txt", publicKey, message)
+
+    ezgmail.send(recipient, "SafeMail", messageEncrypted)
+    console.print("Email sent!")
+
+
+def searchEmails():
+    encryptedMessages = ezgmail.search("label:inbox", maxResults=10)
+    return encryptedMessages
+
+
+def decryptEmails(message, key):
+    try:
+        messageDec = publicKeyCipher.readFromFileAndDecrypt(message, key)
+    except Exception:
+        messageDec = open(message, "r").read()
+    return messageDec
+
+
+def encryptEmails(file, key, message):
+    messageEnc = publicKeyCipher.encryptAndWriteToFile(file, key, message)
+    return messageEnc
+
+
+def makeNewKeys():
+    os.system("clear")
+    console.print(
+        "[bold red]WARNING[/bold red]: This will delete your current keys, so you would be unable to read all your previous conversations, are you sure you want to continue? (y/n)",
+        style="bold red",
+    )
+    if console.input() == "y":
+        date = d.now().strftime("%Y-%m-%d-%H:%M:%S")
+        backupFilePath = Path(f"backup/my_pubkey_backup_{date}.txt")
+        backupFilePath2 = Path(f"backup/my_privkey_backup_{date}.txt")
+        if not os.path.exists("backup"):
+            os.mkdir("backup")
+        shutil.copy(f"my_pubkey.txt", backupFilePath)
+        shutil.copy(f"my_privkey.txt", backupFilePath2)
+        os.remove("my_privkey.txt")
+        os.remove("my_pubkey.txt")
+        makePublicPrivateKeys.makeKeyFiles("my", 1024)
+        os.system("clear")
+        console.print(
+            "Public key: /keys/my_pubkey.txt, private key: /keys/my_privkey.txt",
+            style="bold blue",
+        )
+        console.print(
+            "[bold red]WARNING[/bold red]: Don't share privkey.txt with anyone.",
+            style="bold blue",
+        )
+    else:
+        console.print("Aborting...", style="bold red")
+
+
+def logo():
+    console.print("            __    ___  ___      _ _ ", style="bold cyan")
+    console.print("           / _|   |  \/  |     (_) |", style="bold cyan")
+    console.print(" ___  __ _| |_ ___| .  . | __ _ _| |", style="bold cyan")
+    console.print("/ __|/ _` |  _/ _ \ |\/| |/ _` | | |", style="bold cyan")
+    console.print("\__ \ (_| | ||  __/ |  | | (_| | | |", style="bold cyan")
+    console.print(
+        "|___/\__,_|_| \___\_|  |_/\__,_|_|_|, by [bold red]foglar[bold red]\n",
+        style="bold cyan",
+    )
+
+
+if __name__ == "__main__":
+    main()
